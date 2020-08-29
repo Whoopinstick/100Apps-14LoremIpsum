59d0abeedf1dfd27c2270495fbd59a78bf9967b1
Whoopinstick <k.m.paul68@gmail.com>
2020-08-29 13:47:01 -0400

    initial UI setup

--- 14 - Lorem Ipsum/View/ContentView.swift
+++ 14 - Lorem Ipsum/View/ContentView.swift
@@ -9,15 +9,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    let displayChoices = ["Paragraphs","Words"]
+    @State private var displaySelection = 0
+    @State private var paragraphCount = 0
+    @State private var wordCount = 0
+    
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack {
-                    Text("Hello, World!")
+                    Picker("", selection: $displaySelection) {
+                        ForEach(0..<displayChoices.count) { item in
+                            Text("\(self.displayChoices[item])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .padding(.bottom)
+                    
+                    if displaySelection == 0 {
+                        Stepper("\(paragraphCount) Paragraph(s)", value: $paragraphCount, in: 0...5)
+                        .padding(.bottom)
+                    } else {
+                        Stepper("\(wordCount) Word(s)", value: $wordCount, in: 0...5)
+                        .padding(.bottom)
+                    }
+                    
+                    
+                    Text("The lorem ipsum text goes here")
                 }
-            .padding()
+                .padding()
             }
-        .navigationBarTitle("Lorem Ipsum")
+            .navigationBarTitle("Lorem Ipsum")
         }
     }
 }
